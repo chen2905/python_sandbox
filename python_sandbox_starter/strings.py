6ab6f5e8a89f9509769ca8d288b3c3be74d2879a
Chen Gao <maxlin2905@gmail.com>
2020-08-09 22:30:59 +1000

    s

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,6 +1,27 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = 'CHEN GAO'
+age = 40
+
+#Concatenate
+
+print('hello, my name is '+ name +' and my age is ' + str(age))
+
 
 # String Formatting
 
+
+#Arguments by position
+print ('My name is {name} and I am {age}'.format(name=name, age=age))
+
+#F-strings (3.6+)
+#print (f'Hello, my name is {name} and I am {age}')
 # String Methods
+s = 'hello world'
+print ('#string method'+s.capitalize())
+print ('length:'+ str(len(s)))
+print ('replac:'+ s.replace('world','God'))
+print ('split:'+str(list(s.split())))
+print ('find:'+str(s.find('o')))
+print (' is all alphanumberic:'+str(s.isalnum()))
+print (' is all isalpha:'+str(s.isalpha()))
--- python_sandbox_starter/variables.py
+++ python_sandbox_starter/variables.py
@@ -13,3 +13,18 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+# x = 1           # int
+# y = 2.5         # float
+# name ='Chen'    # str
+# is_cool = True  # bool
+
+x,y,name, is_cool =(1,2.5,'CHEN',True)
+
+
+#Basic math
+a = x+y
+
+#casting
+a = str(a)
+b = float(a)
+print(type(b),b)
